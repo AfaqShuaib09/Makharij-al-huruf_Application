2803d391e92c59fb3fd81ac6ad17836ac0dd3b3d
Muhammad Afaq Shuaib <afaq.shoaib09@gmail.com>
2021-11-09 15:26:36 +0500

    6 screens completed

--- app/src/main/java/com/example/makharijal_hurufapplication/HomeActivity4.java
+++ app/src/main/java/com/example/makharijal_hurufapplication/HomeActivity4.java
@@ -2,19 +2,35 @@ package com.example.makharijal_hurufapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity4 extends AppCompatActivity implements View.OnClickListener{
 
+    Button btnNext, btnSkip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home4);
+
+        btnNext = findViewById(R.id.button);
+        btnNext.setOnClickListener(this);
+
+        btnSkip = findViewById(R.id.button2);
+        btnSkip.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-
+        Intent intent;
+        switch(view.getId()){
+            case R.id.button:
+                intent = new Intent(HomeActivity4.this, HomeActivity5.class);
+                finish();
+                startActivity(intent);
+                break;
+        }
     }
 }
